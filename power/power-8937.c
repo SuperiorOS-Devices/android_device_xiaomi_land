a02ef0dc37ed63f5cf089a3d339dc35cc9883c18
tomascus <slay16@student.monash.edu>
2019-08-05 21:41:17 +0530

    land:power: Release launch boost perflock when launch is completed
    Currently, the launch boost perflock is held for a fixed duration, either
    specified in perfboostsconfig.xml for perf HAL platforms or hardcoded in the
    powerHAL for non-perf HAL platforms.
    
    Using a fixed duration for this perflock has two shortcomings:
    * perflock can be held for too long, causing the CPU freq and other resources
      to be boosted for too long even if the application has finished launching,
      resulting in excessive battery drainage
    * perflock can be held not long enough if it takes more than the timeout
      time to finish launch
    
    The framework sends out a powerhint for both when launch starts and when
    launch ends. The launch finish hint can be used to signal when to release the
    perflock.
    
    Reference: Wahoo power-libperfmgr
    
    Change-Id: I405cc453c5f58d9fb2583b9c6017f3964a0ce024

--- power/power-8937.c
+++ power/power-8937.c
@@ -51,6 +51,7 @@
 #include "performance.h"
 #include "power-common.h"
 
+const int kMaxLaunchDuration = 5000; /* ms */
 const int kMaxInteractiveDuration = 5000; /* ms */
 const int kMinInteractiveDuration = 500; /* ms */
 const int kMinFlingDuration = 1500; /* ms */
@@ -66,9 +67,31 @@ static void process_video_encode_hint(void *metadata);
 static int display_fd;
 #define SYS_DISPLAY_PWR "/sys/kernel/hbtp/display_pwr"
 
-static void process_activity_launch_hint(void *UNUSED(data))
+static int process_activity_launch_hint(void *data)
 {
-    perf_hint_enable_with_type(VENDOR_HINT_FIRST_LAUNCH_BOOST, -1, LAUNCH_BOOST_V1);
+    static int launch_handle = -1;
+    static int launch_mode = 0;
+
+    // release lock early if launch has finished
+    if (!data) {
+        if (CHECK_HANDLE(launch_handle)) {
+            release_request(launch_handle);
+            launch_handle = -1;
+        }
+        launch_mode = 0;
+        return HINT_HANDLED;
+    }
+
+    if (!launch_mode) {
+        launch_handle = perf_hint_enable_with_type(VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                kMaxLaunchDuration, LAUNCH_BOOST_V1);
+        if (!CHECK_HANDLE(launch_handle)) {
+            ALOGE("Failed to perform launch boost");
+            return HINT_NONE;
+        }
+        launch_mode = 1;
+    }
+    return HINT_HANDLED;
 }
 
 static void process_interaction_hint(void *data)
@@ -124,10 +147,7 @@ int  power_hint_override(struct power_module *module, power_hint_t hint,
             return HINT_HANDLED;
 	}
         case POWER_HINT_LAUNCH:
-	{
-            process_activity_launch_hint(data);
-            return HINT_HANDLED;
-	}
+            return process_activity_launch_hint(data);
     }
     return HINT_NONE;
 }
